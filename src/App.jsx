0665f978aa9d1cfcf454b023bf60ac413ecaf9de
SooweKun <Apextoplega@mail.ru>
2024-06-01 11:34:59 +0500

    satoru

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import {Paralax} from './components/paralax/pr.jsx'
+
+export const App = () => {
+
+  return (
+    <Paralax />
+  )
+
+}
---
+++ src/components/paralax/pr.jsx
@@ -0,0 +1,10 @@
+import Styles from './pr.module.css'
+
+export const Paralax = () => {
+
+    return (
+        <div className={Styles.MouseParalax} >
+            <div className={Styles.Bg}></div>
+        </div>
+    )
+}
